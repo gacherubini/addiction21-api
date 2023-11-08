37dc47e241a2580a800fcd1bb58c628f46ab7d1d
Guilherme Abreu Cherubini <guilherme_cherubini@MacBook-Pro-de-Sicredi.local>
2023-11-08 19:50:13 -0300

    adiciona request de access token

--- routes/addicastRotas.js
+++ routes/addicastRotas.js
@@ -4,12 +4,51 @@ const Addicast = require('../models/addicast');
 
 const router = express.Router();
 
+
+const soundCloudApi = "https://api.soundcloud.com"
+const client_id="EKAIa2h9ncPWkdxcZcnDz60W5V3i3XXN"
+const client_secret="Hv5uraDJGc2pMSOI6H3MF2zPoRjOHcnr"
+
+// Obtain access token from SoundCloud API
+
+async function obtainAccessToken() {
+  // POST https://api.soundcloud.com/oauth2/token
+
+  // PARAMS
+  //grant_type=client_credentials
+  //client_id=EKAIa2h9ncPWkdxcZcnDz60W5V3i3XXN
+  //client_secret=Hv5uraDJGc2pMSOI6H3MF2zPoRjOHcnr
+
+  // HEADERS
+  //accept="application/json; charset=utf-8"
+  //Content-Type="application/x-www-form-urlencoded"
+
+
+  // Request SoundCloud API in order to get the access token
+  const response = await axios.post(soundCloudApi + "/oauth2/token", {
+    headers: {
+      'accept': 'application/json; charset=utf-8',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    params : {
+      'grant_type': 'client_credentials',
+      'client_id': client_id,
+      'client_secret': client_secret,
+    }
+  });
+
+  const oauthTokenResponse = await response.data;
+  return oauthTokenResponse.access_token
+}
+
+// List tracks from SoundCloud Addicast playlist
+
 router.get('/', async (req, res) => {
  try {
-    const playlistId = '1012283755'; // Replace with the actual playlist ID
-    const accessToken = '2-294407--wkw13IORLtLcYgUZb87zdkd'; // Replace with the actual access token
+    const playlistId = '1012283755'; // addicast playlist on soundcloud
 
-    const soundCloudEndpoint = `https://api.soundcloud.com/users/${playlistId}/tracks`;
+    const accessToken = obtainAccessToken()
+    const soundCloudEndpoint = soundCloudApi + `/users/${playlistId}/tracks`;
 
     // Make the Axios request to SoundCloud API
     const response = await axios.get(soundCloudEndpoint, {
@@ -35,11 +74,12 @@ router.get('/', async (req, res) => {
  }
 });
 
+// Detail a track from SoundCloud
+
 router.get('/:id', async (req, res) => {
   try {
-     const accessToken = '2-294407--wkw13IORLtLcYgUZb87zdkd'; // Replace with the actual access token
- 
-     const soundCloudEndpoint = `https://api.soundcloud.com/tracks/${req.params.id}`;
+     const accessToken = obtainAccessToken()
+     const soundCloudEndpoint = soundCloudApi + `/tracks/${req.params.id}`;
  
      // Make the Axios request to SoundCloud API
      const response = await axios.get(soundCloudEndpoint, {
