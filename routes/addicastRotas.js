579f0c3b187562ad6053406286ea9a4481053a12
Gabriel <bielcheeeeee@gmail.com>
2023-11-08 19:25:55 -0300

    adionado ids para puxar

--- routes/addicastRotas.js
+++ routes/addicastRotas.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
  try {
     const playlistId = '1012283755'; // Replace with the actual playlist ID
-    const accessToken = '2-294407--ScCanzhO3VRqh4yEikbEjmj'; // Replace with the actual access token
+    const accessToken = '2-294407--wkw13IORLtLcYgUZb87zdkd'; // Replace with the actual access token
 
     const soundCloudEndpoint = `https://api.soundcloud.com/users/${playlistId}/tracks`;
 
@@ -34,5 +34,32 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: 'Failed to retrieve SoundCloud data' });
  }
 });
+
+router.get('/:id', async (req, res) => {
+  try {
+     const accessToken = '2-294407--wkw13IORLtLcYgUZb87zdkd'; // Replace with the actual access token
+ 
+     const soundCloudEndpoint = `https://api.soundcloud.com/tracks/${req.params.id}`;
+ 
+     // Make the Axios request to SoundCloud API
+     const response = await axios.get(soundCloudEndpoint, {
+       headers: {
+         'accept': 'application/json; charset=utf-8',
+         'Authorization': `OAuth ${accessToken}`
+       }
+     });
+ 
+     const soundCloudData = await response.data;
+ 
+     res.status(200).json(soundCloudData)
+ 
+  } catch (error) {
+     console.error(error);
+     res.status(500).json({ error: 'Failed to retrieve SoundCloud data' });
+  }
+ });
+ 
+
+
 
 module.exports = router;
